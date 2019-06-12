ef3ad4da72aabdbe8eb56b72556536c09c421cef
Hannah N <nilagathp@gmail.com>
2019-06-12 21:02:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,31 +1,31 @@
-def instaniate_new_array
-	array.new
+def instantiate_new_array
+  []
 end
 
 def array_with_two_elements
-	[1, 2]
+  [0, 0]
 end
 
 def first_element(array)
-	array[0]
+  array[0]
 end
 
 def third_element(array)
-	array[2]
+  array[2]
 end
 
 def last_element(array)
-	array[-1]
+  array[-1]
 end
 
 def first_element_with_array_methods(array)
-	array.first
+  array.shift
 end
 
 def last_element_with_array_methods(array)
-	array.last
+  array.pop
 end
 
 def length_of_array(array)
-	array.size
+  array.length
 end
